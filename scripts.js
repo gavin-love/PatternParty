e3719374e1ae1fe42796fe4a2a5667f695ee022a
gavin-love <1co.loco@oprotonmail.ch>
2018-08-31 16:31:33 -0600

    Remove scripts files and convert accordion tabs to pure css

--- scripts.js
+++
@@ -1,5 +0,0 @@
-$(".accordion_tabs").on("click", ".accordion_header", function () {
-  const tab = Array.from(this.classList)
-  console.log($(this).parent().next('article').children(`.${tab[1]}`))
-  $(this).parent().next('article').children(`.${tab[1]}`).slideToggle();
-});
