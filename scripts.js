e1c24089eee8edb119b2031cd79a7406166aff16
gavin-love <1co.loco@oprotonmail.ch>
2018-09-19 14:52:29 -0600

    Add media queries

--- scripts.js
+++ scripts.js
@@ -3,4 +3,13 @@ const toggleLinks = (event) => {
   event.preventDefault();
   $('a').slideToggle();
 }
+
+const links = () => {
+  if ($(window).width() > 801) {
+    $('a').css('display', 'block')
+  }
+}
+
+
+$(window).on('resize', links);
 $('.triggerToggle').on('click', toggleLinks);
